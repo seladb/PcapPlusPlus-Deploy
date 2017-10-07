4ec05939720abe394aaa1a71a9f94ec9e25c3248
seladb <seladb@yahoo.com>
2017-10-07 21:33:42 +0300

    Adding vs2015 deployment - first commit

---
+++ deploy-vs2015.bat
@@ -0,0 +1,19 @@
+set compiler=vs2015
+
+set /p LATEST_RELEASE=<C:\projects\pcapplusplus-deploy\latest_release.version
+set DIST_DIR_NAME=pcapplusplus-%LATEST_RELEASE%-windows-%compiler%-%config%-%platform%
+
+move Dist %DIST_DIR_NAME%
+
+copy Deploy\README.release.win.vs2015 %DIST_DIR_NAME%\README.release
+
+mkdir %DIST_DIR_NAME%\ExampleProject
+xcopy Examples\Tutorials\Tutorial-HelloWorld %DIST_DIR_NAME%\ExampleProject /E
+del %DIST_DIR_NAME%\example-app\Makefile.non_windows
+del %DIST_DIR_NAME%\example-app\Makefile.windows
+xcopy C:\projects\pcapplusplus-deploy\PcapPlusPlusPropertySheet.props %DIST_DIR_NAME%\ExampleProject /Y
+:: xcopy C:\projects\pcapplusplus-deploy\ExampleProject.vcxproj %DIST_DIR_NAME%\ExampleProject /Y
+
+7z a -r %DIST_DIR_NAME%.zip %DIST_DIR_NAME%\
+
+curl --upload-file %DIST_DIR_NAME%.zip https://transfer.sh/%DIST_DIR_NAME%.zip
