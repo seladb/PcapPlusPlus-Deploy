4e57653c3ec332d2bb34a574a7e471c8e86d4f54
seladb <seladb@yahoo.com>
2017-10-08 00:54:30 +0300

    Change app name in Makefile + added zip to package-vs2015

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ include ../mk/PcapPlusPlus.mk
 # All Target
 all:
 	g++.exe $(PCAPPP_INCLUDES) -c -o main.o main.cpp
-	g++.exe $(PCAPPP_LIBS_DIR) -static-libgcc -static-libstdc++ -o Tutorial-HelloWorld.exe main.o $(PCAPPP_LIBS)
+	g++.exe $(PCAPPP_LIBS_DIR) -static-libgcc -static-libstdc++ -o example-app.exe main.o $(PCAPPP_LIBS)
 
 # Clean Target
 clean:
 	del main.o
-	del Tutorial-HelloWorld.exe
+	del example-app.exe
--- package-vs2015.bat
+++ package-vs2015.bat
@@ -11,7 +11,8 @@ set X64_RELEASE_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015-Release-x64
 7z x %BASE_DIR%\%X86_RELEASE_NAME%.zip -o"%BASE_DIR%"
 7z x %BASE_DIR%\%X64_RELEASE_NAME%.zip -o"%BASE_DIR%"
 
-set TARGET_DIR=%BASE_DIR%\pcapplusplus-%LATEST_RELEASE%-windows-vs2015
+set TARGET_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015
+set TARGET_DIR=%BASE_DIR%\%TARGET_NAME%
 mkdir %TARGET_DIR%
 
 xcopy %BASE_DIR%\%X86_DEBUG_NAME%\header\* %TARGET_DIR%\header /s /i
@@ -31,3 +32,5 @@ xcopy %BASE_DIR%\%X86_RELEASE_NAME%\*.lib %TARGET_DIR%\x86\Release
 xcopy %BASE_DIR%\%X64_RELEASE_NAME%\*.lib %TARGET_DIR%\x64\Release
 xcopy %BASE_DIR%\%X86_RELEASE_NAME%\examples\* %TARGET_DIR%\x86\examples /s /i
 xcopy %BASE_DIR%\%X64_RELEASE_NAME%\examples\* %TARGET_DIR%\x64\examples /s /i
+
+7z a -r %TARGET_DIR%.zip %TARGET_DIR%\
