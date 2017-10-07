16530955de7cd166a0a7d4b691ef3f57d66d8d10
seladb <seladb@yahoo.com>
2017-10-07 23:49:51 +0300

    Added rest of vs2015 configurations + vs2015 packaging script

---
+++ package-vs2015.bat
@@ -0,0 +1,33 @@
+set BASE_DIR=%1
+set /p LATEST_RELEASE=<latest_release.version
+
+set X86_DEBUG_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015-Debug-x86
+set X64_DEBUG_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015-Debug-x64
+set X86_RELEASE_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015-Release-x86
+set X64_RELEASE_NAME=pcapplusplus-%LATEST_RELEASE%-windows-vs2015-Release-x64
+
+7z x %BASE_DIR%\%X86_DEBUG_NAME%.zip -o"%BASE_DIR%"
+7z x %BASE_DIR%\%X64_DEBUG_NAME%.zip -o"%BASE_DIR%"
+7z x %BASE_DIR%\%X86_RELEASE_NAME%.zip -o"%BASE_DIR%"
+7z x %BASE_DIR%\%X64_RELEASE_NAME%.zip -o"%BASE_DIR%"
+
+set TARGET_DIR=%BASE_DIR%\pcapplusplus-%LATEST_RELEASE%-windows-vs2015
+mkdir %TARGET_DIR%
+
+xcopy %BASE_DIR%\%X86_DEBUG_NAME%\header\* %TARGET_DIR%\header /s /i
+xcopy %BASE_DIR%\%X86_DEBUG_NAME%\ExampleProject\* %TARGET_DIR%\ExampleProject /s /i
+xcopy %BASE_DIR%\%X86_DEBUG_NAME%\README.release %TARGET_DIR%\ /Y
+
+mkdir %TARGET_DIR%\x86\Debug
+mkdir %TARGET_DIR%\x86\Release
+mkdir %TARGET_DIR%\x64\Debug
+mkdir %TARGET_DIR%\x64\Release
+
+xcopy %BASE_DIR%\%X86_DEBUG_NAME%\*.lib %TARGET_DIR%\x86\Debug
+xcopy %BASE_DIR%\%X86_DEBUG_NAME%\*.pdb %TARGET_DIR%\x86\Debug
+xcopy %BASE_DIR%\%X64_DEBUG_NAME%\*.lib %TARGET_DIR%\x64\Debug
+xcopy %BASE_DIR%\%X64_DEBUG_NAME%\*.pdb %TARGET_DIR%\x64\Debug
+xcopy %BASE_DIR%\%X86_RELEASE_NAME%\*.lib %TARGET_DIR%\x86\Release
+xcopy %BASE_DIR%\%X64_RELEASE_NAME%\*.lib %TARGET_DIR%\x64\Release
+xcopy %BASE_DIR%\%X86_RELEASE_NAME%\examples\* %TARGET_DIR%\x86\examples /s /i
+xcopy %BASE_DIR%\%X64_RELEASE_NAME%\examples\* %TARGET_DIR%\x64\examples /s /i
