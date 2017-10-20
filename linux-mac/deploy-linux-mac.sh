3cd302cfdbccc2d047123ebe8cced49764bd51cf
seladb <seladb@yahoo.com>
2017-10-20 16:31:10 -0400

    Some fixes to deploy-linux-mac.sh

--- linux-mac/deploy-linux-mac.sh
+++ linux-mac/deploy-linux-mac.sh
@@ -8,22 +8,16 @@ DIST_DIR_NAME=pcapplusplus-$(cat Deploy/latest_release.version)-$OS_VER-gcc-$(ca
 
 mv Dist $DIST_DIR_NAME
 
-NEW_FIRST_LINE="PCAPPLUSPLUS_HOME := /your/PcapPlusPlus/folder"
-
-sudo sed -i.bak $SED_PARAMS "1s|.*|$NEW_FIRST_LINE|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk && sudo rm $DIST_DIR_NAME/mk/PcapPlusPlus.mk.bak
-
-sudo sed -i.bak $SED_PARAMS "s|"$(PCAPPLUSPLUS_HOME)/Dist"|"$(PCAPPLUSPLUS_HOME)"|" $DIST_DIR_NAME/mk/PcapPlusPlus.mk && sudo rm $DIST_DIR_NAME/mk/PcapPlusPlus.mk.bak
-
 sudo cp ../PcapPlusPlus-Deploy/READMEs/README.release.linux_mac $DIST_DIR_NAME/README.release
 
 sudo mkdir $DIST_DIR_NAME/example-app
 sudo cp Examples/Tutorials/Tutorial-HelloWorld/main.cpp $DIST_DIR_NAME/example-app
 sudo cp Examples/Tutorials/Tutorial-HelloWorld/1_packet.pcap $DIST_DIR_NAME/example-app
 sudo cp ../PcapPlusPlus-Deploy/linux-mac/Makefile.non_windows $DIST_DIR_NAME/example-app/Makefile
+sudo printf "\necho Installation complete!" >> mk/install.sh
+sudo printf "\necho Uninstallation complete!" >> mk/uninstall.sh
 sudo cp mk/install.sh $DIST_DIR_NAME/
 sudo cp mk/uninstall.sh $DIST_DIR_NAME/
-sudo printf "\necho Installation complete!" >> $DIST_DIR_NAME/install.sh
-sudo printf "\necho Uninstallation complete!" >> $DIST_DIR_NAME/uninstall.sh
 
 sudo tar -zcvf $DIST_DIR_NAME.tar.gz $DIST_DIR_NAME/
 curl --upload-file ./$DIST_DIR_NAME.tar.gz https://transfer.sh/$DIST_DIR_NAME.tar.gz
