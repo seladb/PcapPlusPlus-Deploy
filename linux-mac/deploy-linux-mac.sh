173be5c906aabcd8fa0517d0da2e3f1e4c1bb890
seladb <seladb@yahoo.com>
2018-08-06 14:17:17 -0700

    Some fixes to the Linux & Mac dpeloy scripts

--- linux-mac/deploy-linux-mac.sh
+++ linux-mac/deploy-linux-mac.sh
@@ -6,20 +6,33 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then OS_VER=mac-os-$(sw_vers -productVersio
 
 DIST_DIR_NAME=pcapplusplus-$(cat ../PcapPlusPlus-Deploy/misc/latest_release.version)-$OS_VER-gcc-$(cat gcc.version)
 
+# change Dist folder name
 mv Dist $DIST_DIR_NAME
 
+# create README.release
 sudo cp ../PcapPlusPlus-Deploy/READMEs/README.release.header $DIST_DIR_NAME/README.release
 sudo tee -a $DIST_DIR_NAME/README.release < ../PcapPlusPlus-Deploy/READMEs/README.release.linux_mac
 sudo tee -a $DIST_DIR_NAME/README.release < ../PcapPlusPlus-Deploy/READMEs/release_notes.txt
 
+# copy and modify example app
 sudo mkdir $DIST_DIR_NAME/example-app
 sudo cp Examples/Tutorials/Tutorial-HelloWorld/main.cpp $DIST_DIR_NAME/example-app
 sudo cp Examples/Tutorials/Tutorial-HelloWorld/1_packet.pcap $DIST_DIR_NAME/example-app
 sudo cp ../PcapPlusPlus-Deploy/linux-mac/Makefile.non_windows $DIST_DIR_NAME/example-app/Makefile
+
+# modify PcapPlusPlus.mk
+sudo sed -i.bak "s|PCAPPLUSPLUS_HOME :=.*|PCAPPLUSPLUS_HOME := /PcapPlusPlus/Home/Dir|g" $DIST_DIR_NAME/mk/PcapPlusPlus.mk && rm $DIST_DIR_NAME/mk/PcapPlusPlus.mk.bak
+sudo sed -i.bak "s|Dist/||g" $DIST_DIR_NAME/mk/PcapPlusPlus.mk && rm $DIST_DIR_NAME/mk/PcapPlusPlus.mk.bak
+sudo sed -i.bak "s|Dist||g" $DIST_DIR_NAME/mk/PcapPlusPlus.mk && rm $DIST_DIR_NAME/mk/PcapPlusPlus.mk.bak
+
+# copy and modify installation scripts
 sudo cp mk/install.sh $DIST_DIR_NAME/
 sudo cp mk/uninstall.sh $DIST_DIR_NAME/
 printf "\necho Installation complete! " | sudo tee -a $DIST_DIR_NAME/install.sh
 printf "\necho Uninstallation complete! " | sudo tee -a $DIST_DIR_NAME/uninstall.sh
 
+# package everything
 sudo tar -zcvf $DIST_DIR_NAME.tar.gz $DIST_DIR_NAME/
+
+# upload to transfer.sh
 curl --upload-file ./$DIST_DIR_NAME.tar.gz https://transfer.sh/$DIST_DIR_NAME.tar.gz
