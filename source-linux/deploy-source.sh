48344d032319f18039bb45613d592704f50bf42a
seladb <pcapplusplus@gmail.com>
2019-10-31 01:08:01 -0700

    Add package source with Cirrus CI

--- source-linux/deploy-source.sh
+++ source-linux/deploy-source.sh
@@ -3,20 +3,20 @@ set -e # Exit with nonzero exit code if anything fails
 
 DIST_DIR_NAME=pcapplusplus-$(cat misc/latest_release.version)-source-linux
 
-sudo mkdir -p $DIST_DIR_NAME/PcapPlusPlus
+mkdir -p $DIST_DIR_NAME/PcapPlusPlus
 
-sudo rsync -av --exclude='.git' PcapPlusPlus/ $DIST_DIR_NAME/PcapPlusPlus
-sudo cp source-linux/install* $DIST_DIR_NAME/
-sudo cp source-linux/uninstall* $DIST_DIR_NAME/
+rsync -av --exclude='.git' PcapPlusPlus/ $DIST_DIR_NAME/PcapPlusPlus
+cp source-linux/install* $DIST_DIR_NAME/
+cp source-linux/uninstall* $DIST_DIR_NAME/
 
-sudo cp READMEs/README.release.header $DIST_DIR_NAME/README.release
-sudo tee -a $DIST_DIR_NAME/README.release < READMEs/README.release.source
-sudo tee -a $DIST_DIR_NAME/README.release < READMEs/release_notes.txt 
+cp READMEs/README.release.header $DIST_DIR_NAME/README.release
+tee -a $DIST_DIR_NAME/README.release < READMEs/README.release.source
+tee -a $DIST_DIR_NAME/README.release < READMEs/release_notes.txt 
 
-sudo mkdir $DIST_DIR_NAME/example-app
-sudo cp PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld/main.cpp $DIST_DIR_NAME/example-app
-sudo cp PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld/1_packet.pcap $DIST_DIR_NAME/example-app
-sudo cp linux-mac/Makefile.non_windows $DIST_DIR_NAME/example-app/Makefile
+mkdir $DIST_DIR_NAME/example-app
+cp PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld/main.cpp $DIST_DIR_NAME/example-app
+cp PcapPlusPlus/Examples/Tutorials/Tutorial-HelloWorld/1_packet.pcap $DIST_DIR_NAME/example-app
+cp linux-mac/Makefile.non_windows $DIST_DIR_NAME/example-app/Makefile
 
 # package everything
 mkdir package
