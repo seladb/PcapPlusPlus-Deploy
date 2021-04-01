2e54d2078375db289b2f79a5f15a7a637339fa18
seladb <pcapplusplus@gmail.com>
2021-04-01 13:35:51 +0530

    Try to use artifacts to collect binaries and create final package

---
+++ android/deploy-android.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+DIST_DIR_NAME=pcapplusplus-$(cat misc/latest_release.version)-android
+
+# download artifact
+wget https://api.cirrus-ci.com/v1/artifact/build/$CIRRUS_BUILD_ID/temp_package.zip
+
+# change artifact folder name
+mv package $DIST_DIR_NAME
+
+# create README.release
+cp READMEs/README.release.header $DIST_DIR_NAME/README.release
+tee -a $DIST_DIR_NAME/README.release < READMEs/README.release.android
+tee -a $DIST_DIR_NAME/README.release < READMEs/release_notes.txt
+
+# package everything
+mkdir final_package
+tar -zcvf $DIST_DIR_NAME.tar.gz $DIST_DIR_NAME/
+mv $DIST_DIR_NAME.tar.gz final_package/
