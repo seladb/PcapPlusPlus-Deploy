5067f7c064869c559a792caf774576e6bd0818d3
seladb <pcapplusplus@gmail.com>
2022-10-28 02:04:42 -0700

    Drop MinGW32 support + remove pthreads + replace Ubuntu 16.04 with 22.04 (#8)

--- vs/build.bat
+++ vs/build.bat
@@ -1,4 +1,4 @@
-call configure-windows-visual-studio.bat -v %compiler% -w C:\WpdPack -p C:\pthreads
+call configure-windows-visual-studio.bat -v %compiler% -w C:\WpdPack
 
 powershell -command "msbuild mk\%compiler%\PcapPlusPlus.sln /p:Configuration=%config% /p:Platform=%platform% /m"
 if "%config%"=="Release" powershell -command "msbuild mk\%compiler%\PcapPlusPlus-Examples.sln /p:Configuration=%config% /p:Platform=%platform% /m"
